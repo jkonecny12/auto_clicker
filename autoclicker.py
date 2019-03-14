b71c8338701afb8132d52813ac51015a8fdf2a06
Jiri Konecny <jkonecny@redhat.com>
2019-03-14 23:34:42 +0100

    Create a basic structure for clicker
    Read mouse position and window from the mouse.

---
+++ autoclicker.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+import subprocess
+
+
+class XDoTool(object):
+
+    def __init__(self):
+        self._program_name = "xdotool"
+
+    def run_tool(self, args):
+        cmd = [self._program_name]
+        cmd.extend(args)
+        out = subprocess.run(cmd, capture_output=True)
+        out.check_returncode()
+        return out.stdout.decode()
+
+
+class MouseLocator(object):
+
+    def __init__(self):
+        self._runner = XDoTool()
+        self._window = ""
+        self._coordinates = (0, 0)
+
+    @property
+    def window(self):
+        return self._window
+
+    @property
+    def coordinates(self):
+        return self._coordinates
+
+    @property
+    def x(self):
+        return self._coordinates[0]
+
+    @property
+    def y(self):
+        return self._coordinates[1]
+
+    def get_mouse_location(self):
+        out = self._runner.run_tool(['getmouselocation', '--shell'])
+        x = 0
+        y = 0
+
+        for line in out.strip().split('\n'):
+            (key, value) = line.split('=')
+
+            if key == 'X':
+                x = int(value)
+            elif key == 'Y':
+                y = int(value)
+            elif key == "WINDOW":
+                self._window = int(value)
+
+        self._coordinates = (x, y)
+
+
+class MouseController(object):
+
+    def __init__(self):
+        self._runner = XDoTool()
+
+    def set_position(self):
+        pass
+
+
+if __name__ == "__main__":
+    mouse = MouseLocator()
+    mouse.get_mouse_location()
+    print(mouse.coordinates, mouse.window)

4825b20cb77469036eddd56e2abacdb629da53c3
Jiri Konecny <jkonecny@redhat.com>
2019-03-16 19:48:34 +0100

    Do a more iteration of neighbour.py
    I need like 29 iterations so having 3 is too low.

--- neighbour.py
+++ neighbour.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
 
     print(p_next)
 
-    for i in range(0, 3):
+    for i in range(0, 30):
         for y in range(0, 5):
             p_help = Point(p_first.x + (p_space.x * y), p_first.y + (p_space.y * y))
             print(p_help)

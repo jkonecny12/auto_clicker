2bd4e35ffa8cd5faba0687117337f8d2835efffe
Jiri Konecny <jkonecny@redhat.com>
2019-03-16 17:01:09 +0100

    Use new delay solution in neighbour.py

--- neighbour.py
+++ neighbour.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     time.sleep(5)
     locator.get_mouse_location()
     controller = MouseController()
+    controller.delay_before = 0.5
+    controller.delay_after = 0.4
 
     p_next = locator.coordinates
     p_first = Point(p_next.x + 62, p_next.y + 52)
@@ -18,15 +20,11 @@ if __name__ == "__main__":
 
     print(p_next)
 
-    for i in range(0, 5):
+    for i in range(0, 3):
         for y in range(0, 5):
             p_help = Point(p_first.x + (p_space.x * y), p_first.y + (p_space.y * y))
             print(p_help)
             controller.set_position(p_help)
-            time.sleep(0.5)
             controller.left_click()
-            time.sleep(0.2)
         controller.set_position(p_next)
-        time.sleep(0.5)
         controller.left_click()
-        time.sleep(0.2)

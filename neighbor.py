30ee062eac499d489f48f08c888de37d1bcd7a02
Jiri Konecny <jkonecny@redhat.com>
2019-03-16 21:10:38 +0100

    Rename help to support
    It is better naming.

--- neighbor.py
+++ neighbor.py
@@ -10,8 +10,8 @@ def parse_args():
     parser = ArgumentParser(description="Automatic mouse controller to visit all neighbors in one"
                             "game")
 
-    parser.add_argument("action", action="store", type=str, choices=["help", "pub"],
-                        help="""Run script for giving helps or visiting pubs.""")
+    parser.add_argument("action", action="store", type=str, choices=["support", "pub"],
+                        help="""Run script for giving supports or visiting pubs.""")
 
     return parser.parse_args()
 
@@ -29,19 +29,16 @@ def get_position(locator, question):
     return locator.coordinates
 
 
-def click_all_help(locator, controller):
+def click_all_support(locator, controller):
     p_next = get_position(locator, "Move cursor to the next 5 people on the left button")
-    p_first = get_position(locator, "Move cursor to the first portrait HELP button")
-    p_second = get_position(locator, "Move cursor to the second portrait HELP button")
+    p_first = get_position(locator, "Move cursor to the first portrait SUPPORT button")
+    p_second = get_position(locator, "Move cursor to the second portrait SUPPORT button")
     p_space = Point(p_second.x - p_first.x, 0)
-
-    print(p_next)
 
     for i in range(0, 30):
         for y in range(0, 5):
-            p_help = Point(p_first.x + (p_space.x * y), p_first.y + (p_space.y * y))
-            print(p_help)
-            controller.set_position(p_help)
+            p = Point(p_first.x + (p_space.x * y), p_first.y + (p_space.y * y))
+            controller.set_position(p)
             controller.left_click()
         controller.set_position(p_next)
         controller.left_click()
@@ -55,5 +52,5 @@ if __name__ == "__main__":
     controller.delay_before = 0.5
     controller.delay_after = 0.4
 
-    if ns.action == "help":
-        click_all_help(locator, controller)
+    if ns.action == "support":
+        click_all_support(locator, controller)
